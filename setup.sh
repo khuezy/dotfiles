16592c37df98856c57db8e8bcca8ba0fadd5410f
Aeden Jameson <aeden.jameson@gmail.com>
2013-04-15 17:09:02 -0700

    update setup file

--- setup.sh
+++ setup.sh
@@ -5,6 +5,8 @@ ln -s ~/dotfiles/gitconfig ~/.gitconfig
 mkdir ~/.bash
 ln -s ~/dotfiles/bash/rbenv ~/.bash/rbenv
 ln -s ~/dotfiles/bash/aliases ~/.bash/aliases
+ln -s ~/dotfiles/bash/chef ~/.bash/chef
+ln -s ~/dotfiles/bash/aws ~/.bash/aws
 ln -s ~/dotfiles/bash-git-prompt/gitstatus.py ~/.bash/gitstatus.py
 ln -s ~/dotfiles/bash-git-prompt/gitprompt.sh ~/.bash/gitprompt.sh
 ln -s ~/dotfiles/git-completion.bash ~/.bash/git-completion.bash
